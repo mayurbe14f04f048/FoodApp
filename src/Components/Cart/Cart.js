5936a3478309ff04a27d541922e391bc4b0a5d97
Mayur <mvtmayurtaksande@gmail.com>
2021-07-23 21:24:38 +0530

    first commit

---
+++ src/Components/Cart/Cart.js
@@ -0,0 +1,50 @@
+import classes from "./Cart.module.css";
+import Modal from "../UI/Modal";
+import CartItem from "./CartItem";
+import { useContext } from "react";
+import cartContext from "../../store/cart-context";
+
+const Cart = (props) => {
+  const cartCtx = useContext(cartContext);
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+
+  const hasItem = cartCtx.items.lenght > 0;
+
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
+
+  const cartItems = (
+    <ul className={classes["cart-item"]}>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove = {cartItemRemoveHandler.bind(null,item.id)}
+          onAdd = {cartItemAddHandler.bind(null,item)}
+        />
+      ))}
+    </ul>
+  );
+  return (
+      <Modal onClose = {props.onClose}>
+          {cartItems}
+          <div className={classes.total}>
+              <span>Total Aount</span>
+              <span>{totalAmount}</span>
+          </div>
+          <div className={classes.actions}>
+              <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
+              {hasItem && <button className={classes.button}>Order</button>}
+          </div>
+      </Modal>
+  )
+};
+
+export default Cart;
---
+++ src/Components/Meals/Meals.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+import MealsSummery from "./MealsSummery";
+import AvailableMeals from "./AvailableMeals";
+
+const Meals = () => {
+  return (
+    <Fragment>
+      <MealsSummery />
+      <AvailableMeals />
+    </Fragment>
+  );
+};
+
+export default Meals;
